50c8c80e528a08704c103687b704bbf746cd3aa6
nekkochan0x0007 <nekkochan0x0007@tutamail.com>
2025-07-16 06:06:17 +0700

    [published] Release version 1.0.0.2

--- paimonDotMoeCompanion/Program.cs
+++ paimonDotMoeCompanion/Program.cs
@@ -17,8 +17,39 @@ namespace paimonDotMoeCompanion
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            AutoUpdater.Start("https://https://duy-thanh.github.io/paimonDotMoeCompanion/update.xml");
+            AutoUpdater.Start("https://duy-thanh.github.io/paimonDotMoeCompanion/update.xml");
+            AutoUpdater.Mandatory = false; // set to true if force update
+            AutoUpdater.ShowSkipButton = false;
+            AutoUpdater.ReportErrors = true;
+
+            // Optional: Handle update events
+            //AutoUpdater.CheckForUpdateEvent += AutoUpdater_CheckForUpdateEvent;
             Application.Run(new Form1());
         }
+
+        //private static void AutoUpdater_CheckForUpdateEvent(UpdateInfoEventArgs args)
+        //{
+        //    if (args.IsUpdateAvailable)
+        //    {
+        //        DialogResult dialogResult = MessageBox.Show(
+        //            $"New version {args.CurrentVersion} is available (you have {args.InstalledVersion}).\n\nDo you want to update the application now?",
+        //            "Update Available",
+        //            MessageBoxButtons.YesNo,
+        //            MessageBoxIcon.Information
+        //        );
+
+        //        if (dialogResult == DialogResult.Yes)
+        //        {
+        //            try
+        //            {
+        //                AutoUpdater.DownloadUpdate(args);
+        //            }
+        //            catch (Exception ex)
+        //            {
+        //                MessageBox.Show("Update failed: " + ex.Message);
+        //            }
+        //        }
+        //    }
+        //}
     }
 }
